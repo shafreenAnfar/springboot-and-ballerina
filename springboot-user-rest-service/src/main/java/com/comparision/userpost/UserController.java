fd7c3d629afe0557ce565c924505e23a1930b47b
shafreenAnfar <www.call911@gmail.com>
2023-01-24 09:48:50 +0530

    Add openAPI documentation

--- springboot-user-rest-service/src/main/java/com/comparision/userpost/UserController.java
+++ springboot-user-rest-service/src/main/java/com/comparision/userpost/UserController.java
@@ -1,11 +1,14 @@
 package com.comparision.userpost;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+import java.awt.print.Book;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,6 +39,35 @@ public class UserController {
         return user.get();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Create user",
+                    content = { @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = User.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = { @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = ErrorDetails.class))})
+    })
+    @PostMapping("/medium/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        repository.save(user);
+        return ResponseEntity.created(null).build();
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Delete user")
+    })
+    @DeleteMapping("/medium/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        repository.deleteById(id);
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Create post",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Post.class))}),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))})})
     @GetMapping("/medium/users/{id}/posts")
     public List<Post> retrieveUserPosts(@PathVariable int id) {
         Optional<User> user = repository.findById(id);
@@ -46,21 +78,14 @@ public class UserController {
         return user.get().getPosts();
     }
 
-    @PostMapping("/medium/users")
-    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
-        User savedUser = repository.save(user);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(savedUser.getId())
-                .toUri();
-        return ResponseEntity.created(location).build();
-    }
-
-    @DeleteMapping("/medium/users/{id}")
-    public void deleteUser(@PathVariable int id) {
-        repository.deleteById(id);
-    }
-
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Create post",
+                    content = { @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = User.class))}),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = { @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = ErrorDetails.class))})
+    })
     @PostMapping("/medium/users/{id}/post")
     public ResponseEntity<User> createUserPost(@PathVariable int id, @Valid @RequestBody Post post) {
         Optional<User> user = repository.findById(id);
@@ -69,14 +94,8 @@ public class UserController {
             throw new UserNotFoundException("id: " + id);
 
         post.setUser(user.get());
+        postRepository.save(post);
 
-        Post savedPost = postRepository.save(post);
-
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(savedPost.getId())
-                .toUri();
-
-        return ResponseEntity.created(location).build();
+        return ResponseEntity.created(null).build();
     }
 }

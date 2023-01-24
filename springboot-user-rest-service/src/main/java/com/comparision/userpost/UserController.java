be14b6cc7c6e3d2372586dbfe3e0fd1e9314ab62
shafreenAnfar <www.call911@gmail.com>
2023-01-24 09:49:57 +0530

    Remove unused imports

--- springboot-user-rest-service/src/main/java/com/comparision/userpost/UserController.java
+++ springboot-user-rest-service/src/main/java/com/comparision/userpost/UserController.java
@@ -8,7 +8,6 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Book;
 import java.util.List;
 import java.util.Optional;
 

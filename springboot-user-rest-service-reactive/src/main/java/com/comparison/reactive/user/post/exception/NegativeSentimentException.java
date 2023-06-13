d46c6fa411461e6332aced800f58f74c2bfc8425
ayeshLK <ayesh9303@gmail.com>
2023-06-13 15:23:09 +0530

    Remove unwanted configurations

--- springboot-user-rest-service-reactive/src/main/java/com/comparison/reactive/user/post/exception/NegativeSentimentException.java
+++ springboot-user-rest-service-reactive/src/main/java/com/comparison/reactive/user/post/exception/NegativeSentimentException.java
@@ -1,9 +1,5 @@
 package com.comparison.reactive.user.post.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.FORBIDDEN)
 public class NegativeSentimentException extends RuntimeException {
     public NegativeSentimentException(String message) {
         super(message);
--- springboot-user-rest-service-reactive/src/main/java/com/comparison/reactive/user/post/exception/UserNotFoundException.java
+++ springboot-user-rest-service-reactive/src/main/java/com/comparison/reactive/user/post/exception/UserNotFoundException.java
@@ -1,9 +1,5 @@
 package com.comparison.reactive.user.post.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
         super(message);
